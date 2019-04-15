12be9de58c9bafb30c694eff6cdc83539bd77f30
karol.mendza <karol.mendza@payarto.com>
2019-04-15 10:40:51 +0200

    Implemented ngFor and ngIf as well as styling and complex object handling

--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-event',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event.component.scss']
 })
 export class EventComponent implements OnInit {
+
+  @Input() value: any;
 
   constructor() { }
 
